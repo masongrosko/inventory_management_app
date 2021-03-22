1a01d6fa6e1dd81fc3f0ef4a7ffbcc12bb9e4cf3
masongrosko <69281238+masongrosko@users.noreply.github.com>
2021-03-22 01:15:25 -0500

    prevent null entries to inventory

--- app/src/main/java/com/example/project_two_grosko/AddInventoryActivity.java
+++ app/src/main/java/com/example/project_two_grosko/AddInventoryActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -38,36 +39,38 @@ public class AddInventoryActivity extends AppCompatActivity {
         inventoryTableHelper = new InventoryTableHelper(this);
 
         acceptButton.setOnClickListener(v -> {
-            if (id.length() != 0) {
+            if (
+                (id.length() != 0)
+                && (name.length() != 0)
+                && (stock.length() != 0)
+                && (price.length() != 0)
+            ) {
                 id_string = id.getText().toString();
-            }
-            if (name.length() != 0) {
                 name_string = name.getText().toString();
-            }
-            if (stock.length() != 0) {
                 stock_string = stock.getText().toString();
-            }
-            if (price.length() != 0) {
                 price_string = price.getText().toString();
-            }
-            Log.d(
-                    "AddInventoryActivity",
-                    "onClick: " + username + id_string + name_string + stock_string + price_string
-            );
+                Log.d(
+                        "AddInventoryActivity",
+                        "onClick: " + username + id_string + name_string + stock_string + price_string
+                );
 
-            inventoryTableHelper.SetRow(
-                    username,
-                    id_string,
-                    name_string,
-                    stock_string,
-                    price_string
-            );
-            Intent inventoryManagementIntent = new Intent(
-                    AddInventoryActivity.this,
-                    InventoryManagementActivity.class
-            );
-            inventoryManagementIntent.putExtra("username", username);
-            startActivity(inventoryManagementIntent);
+                inventoryTableHelper.SetRow(
+                        username,
+                        id_string,
+                        name_string,
+                        stock_string,
+                        price_string
+                );
+                Intent inventoryManagementIntent = new Intent(
+                        AddInventoryActivity.this,
+                        InventoryManagementActivity.class
+                );
+                inventoryManagementIntent.putExtra("username", username);
+                startActivity(inventoryManagementIntent);
+            }
+            else {
+                Toast.makeText(this, "All rows must contain data", Toast.LENGTH_SHORT).show();
+            }
         });
 
     }
